95dea051bc1d607331ebac0d572320cf19c5a9ce
Zainul-Malik <119996723+Zainul-Malik@users.noreply.github.com>
2022-12-14 18:47:23 -0700

    Delete Assignment 3 directory

--- Assignment 3/asn-js-PROVIDED/scripts/contact.js
+++
@@ -1,8 +0,0 @@
-// when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
-document.getElementById("submit-button").addEventListener("click",textChange);
-document.getElementById("contact-page").style.fontSize="24px";
-function textChange(){
-    document.getElementById("contact-page").innerHTML="Thank you for your message!";  
-}
-// hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
-
